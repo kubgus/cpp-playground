0e70da616d369dd276c0e4655b0cd2b23fcb660e
kubgus <ja.gustafik@outlook.sk>
2023-11-30 19:26:00 +0100

    Optimize bounce simulation

--- bounce-simulation.cpp
+++ bounce-simulation.cpp
@@ -1,49 +1,48 @@
 #include <iostream>
-#include <cmath>
-#include <cstring>
 
 struct Point
 {
-    // [position] = m
+    // x: position
     double x;
-    // [velocity] = ms^-1
+    // v: velocity [m/s]
     double v;
-    // [mass] = kg
-    long long m;
+    // m: mass [kg]
+    long double m;
 
-    Point(double x, double v, long double m) : x(x), v(v), m(m) {}
+    constexpr Point(double x, double v, long double m) : x(x), v(v), m(m) {}
 };
 
-inline void print(const long long &t, const int &b, const Point &left, const Point &right)
+inline void print(long long t, int b, const Point &left, const Point &right)
 {
     long y = t / 31536000, m = (t % 31536000) / 2592000, d = (t % 2592000) / 86400, h = (t % 86400) / 3600, min = (t % 3600) / 60, s = t % 60;
-    std::cout << "Time: " << y << "y " << m << "m " << d << "d " << h << "h " << min << "min " << s << "s" << std::endl
-              << "Bounces: " << b << std::endl
-              << "Left: " << left.x << "m, " << left.v << "ms^-1" << std::endl
-              << "Right: " << right.x << "m, " << right.v << "ms^-1" << std::endl
-              << std::endl;
+    std::cout << "Time: " << y << "y " << m << "m " << d << "d " << h << "h " << min << "min " << s << "s\n"
+              << "Bounces: " << b << "\n"
+              << "Left: " << left.x << "m, " << left.v << "m/s\n"
+              << "Right: " << right.x << "m, " << right.v << "m/s\n\n";
 }
 
-int main(int argc, char **argv)
+int main()
 {
     Point left(1, 0, 1);
     Point right(2, -1, 100000000);
 
-    // [time] = s
+    // t: time [s]
     long long t = 0;
-    // [bounces]
+    // b: bounces
     int b = 0;
 
     while (left.v < 0 || left.v > right.v)
     {
         left.x += left.v;
         right.x += right.v;
+
         if (left.x <= 0)
         {
             left.x = 0;
             left.v = -left.v;
             b++;
         }
+
         if (left.x >= right.x)
         {
             right.x = left.x;
@@ -57,12 +56,13 @@ int main(int argc, char **argv)
 
             b++;
         }
+
         if (t % 1000000000 == 0)
             print(t, b, left, right);
+
         t++;
     }
 
-    std::cout << "===================================" << std::endl
-              << std::endl;
+    std::cout << "===================================\n\n";
     print(t, b, left, right);
 }
