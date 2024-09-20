b12c29525dedfd0ecdaff5ebc92f2c8341ef0ccb
kubgus <ja.gustafik@outlook.sk>
2024-09-20 13:29:57 +0200

    Replace unnecessarily complex types (uint_32t -> unsigned int)

--- dice-tool.cpp
+++ dice-tool.cpp
@@ -1,4 +1,3 @@
-#include <cstdint>
 #include <iostream>
 #include <vector>
 
@@ -8,29 +7,31 @@
 // my knowledge of C++ and algorithms.
 // May be faulty - edge cases not tested yet!
 
+using uint = unsigned int;
+
 struct dice {
-    uint32_t sides = 6;
+    uint sides = 6;
 };
 
-uint32_t total_outcome_count(std::vector<dice> dice_set) {
-    int count = 1;
+uint total_outcome_count(std::vector<dice> dice_set) {
+    uint count = 1;
     for (dice d : dice_set) {
         count *= d.sides;
     }
     return count;
 }
 
-uint32_t good_outcome_count(
-    const std::vector<dice>& dice_set, int aim,
-    int value_until_now = 0, int index = 0
+uint good_outcome_count(
+    const std::vector<dice>& dice_set, uint aim,
+    uint value_until_now = 0, uint index = 0
 ) {
     dice d1 = dice_set[index];
     if (index + 1 > dice_set.size()) {
         return d1.sides <= aim ? 1 : 0;
     }
     dice d2 = dice_set[index + 1];
-    int count = 0;
-    for (int i = 1; i <= d1.sides; i++) {
+    uint count = 0;
+    for (uint i = 1; i <= d1.sides; i++) {
         if (aim - (value_until_now + i) <= d2.sides) {
             count += good_outcome_count(
                 dice_set, aim, value_until_now + i, index + 1
@@ -40,9 +41,9 @@ uint32_t good_outcome_count(
     return count;
 }
 
-inline float chance_of(std::vector<dice> dice_set, int number) {
-    uint32_t total_outcomes = total_outcome_count(dice_set);
-    uint32_t good_outcomes = good_outcome_count(dice_set, number);
+inline float chance_of(std::vector<dice> dice_set, uint number) {
+    uint total_outcomes = total_outcome_count(dice_set);
+    uint good_outcomes = good_outcome_count(dice_set, number);
     return (float)good_outcomes / (float)total_outcomes;
 }
 
@@ -52,15 +53,15 @@ inline float to_percent(float number) {
 
 int main() {
     std::cout << "Number of dice: ";
-    uint32_t dice_amt;
+    uint dice_amt;
     std::cin >> dice_amt;
 
     std::vector<dice> dice_set;
     dice_set.reserve(dice_amt);
 
-    for (int i = 0; i < dice_amt; i++) {
+    for (uint i = 0; i < dice_amt; i++) {
         std::cout << "Sides of D" << i + 1 << ": ";
-        uint32_t sides;
+        uint sides;
         std::cin >> sides;
 
         dice_set.push_back(dice { sides });
